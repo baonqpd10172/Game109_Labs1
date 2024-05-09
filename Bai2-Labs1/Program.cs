54d80f90e2e99f1270f3fa01cec27fd4e18c7b5e
baonqpd10172 <162962147+baonqpd10172@users.noreply.github.com>
2024-05-09 22:44:54 +0700

    Add files via upload

---
+++ Bai2-Labs1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    public class Maps
+    {
+        public string id;
+        public string userName;
+        public static string _inGame = "lien minh huyen thoai";
+
+        public Maps(string id, string userName)
+        {
+            this.id = id;
+            this.userName = userName;
+        }
+        public string ShowData()
+        {
+            return "ID : " + id + "|Name:" + userName + "|inGame:" + _inGame;
+        }
+    }
+
---
+++ Slide1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+public static class UserData
+{
+    public static string id;
+    public static string userName;
+    public static float gold;
+    public static int point;
+
+    static UserData()
+    {
+        id = "1st";
+        userName = "first";
+        gold = 0;
+        point = 0;
+    }
+    public static void ShowData()
+    {
+        Console.WriteLine("Id:" + id);
+        Console.WriteLine("UserName:" + userName);
+
+    }
+
+}
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        UserData.ShowData();
+        return 0;
+    }
+}
+
